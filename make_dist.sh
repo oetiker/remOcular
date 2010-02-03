b1023eb4ef999f0c2b2b1c4c9745837a536cc35b
Tobias Oetiker <tobi@oetiker.ch>
2010-02-03 14:39:53 +0000

    initial

--- make_dist.sh
+++ make_dist.sh
@@ -9,12 +9,13 @@ A=remocular
 ROOT=`dirname $0`
 url=`svn info $ROOT | grep URL | sed 's/.* //'`
 svn export $url $A-$V
-cd $A-$V/client
-ln -s ../../../../../../../usr/pack/qooxdoo-0.8svn-to/frontend qooxdoo
+cd $A-$V
 perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;s/#DATE#/$D/g;" installer.sh client/Manifest.json README COPYRIGHT
+cd client
+ln -s ../../../../../../../usr/pack/qooxdoo-0.8svn-to/frontend qooxdoo
 ./generate.py build
-perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;" client/build/script/remocular.js
+perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;" build/script/remocular.js
 rm qooxdoo
 cd ../..
 tar vzcf $A-$V.tar.gz $A-$V
-rm -rf $A-$V
+rm -r $A-$V
