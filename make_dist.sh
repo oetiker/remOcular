a21da71e18359700b949bb8b3a16e49bec6645cc
Tobias Oetiker <tobi@oetiker.ch>
2010-02-03 15:12:27 +0000

    fix dist builder

--- make_dist.sh
+++ make_dist.sh
@@ -6,13 +6,13 @@ V=1.0.0
 Y=`date +%Y`
 D=`date "+%Y-%m-%d"`
 A=remocular
-ROOT=`dirname $0`
-url=`svn info $ROOT | grep URL | sed 's/.* //'`
+ROOT=`dirname "$0"`
+url=`svn info "$ROOT" | grep URL | sed 's/.* //'`
 
-[ -d $A-$V ] && rm -r $A-$V
+[ -d "$A-$V" ] && rm -r "$A-$V"
+svn export "$url" "$A-$V"
 
-svn export $url $A-$V
-cd $A-$V
+cd "$A-$V"
 perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;s/#DATE#/$D/g;" installer.sh client/Manifest.json README COPYRIGHT
 cd client
 ln -s ../../../../../../../usr/pack/qooxdoo-0.8svn-to/frontend qooxdoo
@@ -20,5 +20,5 @@ ln -s ../../../../../../../usr/pack/qooxdoo-0.8svn-to/frontend qooxdoo
 perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;" build/script/remocular.js
 rm qooxdoo
 cd ../..
-tar vzcf $A-$V.tar.gz $A-$V
-rm -r $A-$V
+tar vzcf "$A-$V".tar.gz "$A-$V"
+rm -r "$A-$V"
