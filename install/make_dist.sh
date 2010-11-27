da59d347a69a18a1e46d3a5a3746298cbf155f0b
Tobias Oetiker <tobi@oetiker.ch>
2010-11-27 11:49:41 +0100

    make install and distribution process location independent

--- install/make_dist.sh
+++ install/make_dist.sh
@@ -2,22 +2,24 @@
 # $Id$
 # Distribution Maker
 set -e
+if [ "x${QOOXDOO_PATH:=$1}" = x ]; then
+   echo usage: $0 qooxdoo-sdk-path
+   echo    or set QOOXDOO_PATH
+   exit 1
+fi
 V=1.0.0
 Y=`date +%Y`
 D=`date "+%Y-%m-%d"`
 A=remocular
 ROOT=`dirname "$0"`/..
-url=`svn info "$ROOT" | grep URL | sed 's/.* //'`
-[ -d "$A-$V" ] && rm -r "$A-$V"
-svn export "$url" "$A-$V"
-
-cd "$A-$V"
-perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;s/#DATE#/$D/g;" installer.sh client/Manifest.json README COPYRIGHT
-cd client
-ln -s ../../../../../../../usr/pack/qooxdoo-0.8svn-to/frontend qooxdoo
-./generate.py build
+BUILD=/tmp/${USER}_QX_BUILD/${A}-${V}
+mkdir -p $BUILD
+cp -rp $ROOT/* $BUILD
+cd $BUILD
+perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;s/#DATE#/$D/g;" installer.sh frontend/Manifest.json README COPYRIGHT
+cd frontend
+$QOOXDOO_PATH/tool/bin/generator.py -m QOOXDOO_PATH:$QOOXDOO_PATH -m CACHE:${TMP:-/tmp}/${USER}_QX_CACHE build
 perl -i -p -e "s/#VERSION#/$V/g;s/#YEAR#/$Y/g;" build/script/remocular.js
-rm qooxdoo
 cd ../..
-tar vzcf "$A-$V".tar.gz "$A-$V"
-rm -r "$A-$V"
+tar vzcf "$ROOT/$A-$V".tar.gz  --exclude "*~" --exclude .git "$A-$V"
+rm -r $BUILD
