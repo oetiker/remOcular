46607d2ee1afd9ec079d7aa72334881c1a002a9b
Roman Plessl <roman.plessl@oetiker.ch>
2011-10-17 22:40:54 +0800

    updated license to gpl v3

--- backend/lib/remOcular/Plugin/Top.pm
+++ backend/lib/remOcular/Plugin/Top.pm
@@ -190,23 +190,22 @@ __END__
 
 =head1 COPYRIGHT
 
-Copyright (c) 2008 by OETIKER+PARTNER AG. All rights reserved.
+Copyright (c) 2009-2011 by OETIKER+PARTNER AG. All rights reserved.
 
 =head1 LICENSE
 
-This program is free software; you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 =head1 AUTHOR
 
@@ -214,8 +213,7 @@ S<Tobias Oetiker E<lt>tobi@oetiker.chE<gt>>
 
 =head1 HISTORY
 
- 2008-04-24 to 1.0 first version for OSP
- 2008-10-07 to 1.1 re-used for THD
+ 2009-11-06 to 1.0 first version 
 
 =cut
 
